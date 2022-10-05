42e8e5677a31c31c82abaf058677452f166a3d91
KoKwanwun <dkfajflsla@naver.com>
2022-10-05 10:26:42 +0900

    CalculatorMain 매개변수 수정

--- src/week3_221004_221007/day_221005/CalculatorMain.java
+++ src/week3_221004_221007/day_221005/CalculatorMain.java
@@ -4,9 +4,9 @@ public class CalculatorMain {
     public static void main(String[] args) {
         Calculator c = new Calculator();
 
-        c.plus(2, 3);
-        c.minus(2, 3);
-        c.multiple(2, 3);
-        c.divide(2, 3);
+        c.plus(10, 20);
+        c.minus(10, 20);
+        c.multiple(10, 20);
+        c.divide(10, 20);
     }
 }
