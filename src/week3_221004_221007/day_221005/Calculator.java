df72b10c104dce92dc892719fed351f10650dd5e
KoKwanwun <dkfajflsla@naver.com>
2022-10-05 10:23:39 +0900

    221005 1. Calaulator 추가(+, -, *, /) 2. CalaulatorMain 추가

---
+++ src/week3_221004_221007/day_221005/Calculator.java
@@ -0,0 +1,19 @@
+package week3_221004_221007.day_221005;
+
+public class Calculator {
+    public void plus(int a, int b) {
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+
+    public void minus(int a, int b) {
+        System.out.println(a + " - " + b + " = " + (a - b));
+    }
+
+    public void multiple(int a, int b) {
+        System.out.println(a + " * " + b + " = " + (a * b));
+    }
+
+    public void divide(int a, int b) {
+        System.out.println(a + " / " + b + " = " + ((double)a / b));
+    }
+}
---
+++ src/week3_221004_221007/day_221005/CalculatorMain.java
@@ -0,0 +1,12 @@
+package week3_221004_221007.day_221005;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        c.plus(2, 3);
+        c.minus(2, 3);
+        c.multiple(2, 3);
+        c.divide(2, 3);
+    }
+}
