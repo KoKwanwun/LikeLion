bd1da07c9e95a6350537e2744752c7f70d9677ef
KoKwanwun <dkfajflsla@naver.com>
2022-10-06 15:11:11 +0900

    ReadFile 수정 (파일 목록 출력)

--- src/week3_221004_221007/day_221006/File/ReadFile.java
+++ src/week3_221004_221007/day_221006/File/ReadFile.java
@@ -1,5 +1,7 @@
 package week3_221004_221007.day_221006.File;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFile {
@@ -11,5 +13,18 @@ public class ReadFile {
         System.out.println(readFileByByte.readTwoByte());
 
         System.out.println(readFileByByte.readNByte(10));
+
+        // FileReader 설명
+        FileReader fileReader = new FileReader("./b_file.txt");
+
+        // 디렉토리로 파일 접근
+        File dir = new File("./");  // new File로 디렉토리 설정
+
+        File files[] = dir.listFiles(); // 현재 위치의 파일들이 담김
+        System.out.println(files[0]);   // 첫 파일 출력
+
+        for(int i = 0; i < files.length; i++){
+            System.out.println(files[i]);
+        }
     }
 }
