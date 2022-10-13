13ef1c466da1df0bda58010fb5602d9e5d9bad00
KoKwanwun <dkfajflsla@naver.com>
2022-10-13 13:12:40 +0900

    Hospital Project 수정 (address 파싱 추가)

--- src/main/java/com/line/domain/Hospital.java
+++ src/main/java/com/line/domain/Hospital.java
@@ -2,12 +2,28 @@ package com.line.domain;
 
 public class Hospital {
     private String id;
+    private String address;
+    private String district;
+    private String category;
+    private int emergencyRoom;
+    private String name;
+    private String subdivision;
+
 
     public Hospital(String id) {
-        this.id = id.replaceAll("\"", "");
+        this.id = id;
+    }
+
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
     }
 
     public String getId() {
         return id;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
