a956ea982aec9cd3e2c612622d0e1b90e391c009
KoKwanwun <dkfajflsla@naver.com>
2022-10-13 14:49:51 +0900

    Hospital Project 수정 (subdivision 파싱 일부 수정, Parsing 추상화 및 테스트케이스 추가)

--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -3,28 +3,25 @@ package com.line.parser;
 import com.line.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital> {
+    private String getSubdivision(String name) {
+        String[] subdivisions = {"소아과", "피부과", "성형외과", "정형외과", "척추", "교정", "산부인과", "관절", "봉합", "화상", "골절", "영유아", "안과", "가정의학과", "비뇨기과", "치과", "내과", "외과"};
+
+        for (String subdivision : subdivisions) {
+            if(name.contains(subdivision)){
+                return subdivision;
+            }
+        }
+        return "";
+    }
+
     @Override
     public Hospital parse(String str) {
         str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
 
-        String subDivision = setSubdivision(splitted[10]);
+        String name = splitted[10];
+        String subDivision = getSubdivision(name);
 
-        return new Hospital(splitted[0], splitted[1], splitted[2], Integer.parseInt(splitted[6]), splitted[10], subDivision);
-    }
-
-    String setSubdivision(String name) {
-        String result = "";
-        String[] inputChecks = {"내과", "외과", "소아", "피부", "성형", "정형외과", "척추", "교정", "산부인과", "관절", "봉합", "화상", "골절", "영유아", "안과", "가정의학과", "비뇨기과", "치과"};
-        for (String inputCheck : inputChecks) {
-            if(name.contains(inputCheck)){
-                result += inputCheck;
-            }
-        }
-        if(result == ""){
-            result = null;
-        }
-
-        return result;
+        return new Hospital(splitted[0], splitted[1], splitted[2], Integer.parseInt(splitted[6]), name, subDivision);
     }
 }
