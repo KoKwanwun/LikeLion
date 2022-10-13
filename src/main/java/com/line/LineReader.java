350f56a95e131540f80ee892481641b97c98f8e0
KoKwanwun <dkfajflsla@naver.com>
2022-10-13 11:39:55 +0900

    Hospital Project 추가 (id가 파싱이 잘 되는지 테스트)

---
+++ src/main/java/com/line/LineReader.java
@@ -0,0 +1,41 @@
+package com.line;
+
+import com.line.parser.Parser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineReader<T> {
+    Parser<T> parser;
+
+    boolean isRemoveColumnName = true;
+
+    public LineReader(Parser<T> parser) {
+        this.parser = parser;
+    }
+
+    // 첫행 받는여부를 생성자로 받을 수 있음
+    public LineReader(Parser<T> parser, boolean isRemoveColumnName) {
+        this.parser = parser;
+        this.isRemoveColumnName = isRemoveColumnName;
+    }
+
+    List<T> readlines(String filename) throws IOException {
+        List<T> result = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String str;
+
+        // 첫 행 날리기
+        if(isRemoveColumnName) {
+            br.readLine();
+        }
+
+        while((str = br.readLine()) != null) {
+            result.add(parser.parse(str));
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/line/Main.java
@@ -0,0 +1,21 @@
+package com.line;
+
+import com.line.domain.Hospital;
+import com.line.parser.HospitalParser;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        LineReader<Hospital> hospitalLineReader = new LineReader<>(new HospitalParser());
+        String filename = "D:\\고관운 자료\\멋쟁이사자처럼\\백엔드스쿨 2기 교본\\220919\\4주차\\서울시 병의원 위치 정보.csv";
+        List<Hospital> hospitals = hospitalLineReader.readlines(filename);
+
+        System.out.println(hospitals.size());
+
+        for (Hospital hospital : hospitals) {
+            System.out.println(hospital.getId());
+        }
+    }
+}
