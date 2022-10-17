8d5ca771722e26a5c7c534bef293a3047334fcbf
KoKwanwun <dkfajflsla@naver.com>
2022-10-17 09:11:48 +0900

    Hospital_Project에서 테스트한 것 파일 이동

--- src/week4_20221011_20221014/day_221012/Main.java
+++ src/week4_20221011_20221014/day_221012/Main.java
@@ -4,6 +4,7 @@ import week4_20221011_20221014.day_221012.domain.Hospital;
 import week4_20221011_20221014.day_221012.parser.HospitalParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -14,8 +15,12 @@ public class Main {
 
         System.out.println(hospitals.size());
 
+        List<String> lines = new ArrayList<>();
         for (Hospital hospital : hospitals) {
-            System.out.println(hospital.getId());
+            lines.add(hospital.getSqlInsertQuery());
         }
+        String sqlFilename = "hospital_insert.sql";
+        hospitalLineReader.createANewFile(sqlFilename);
+        hospitalLineReader.writeLines(lines, sqlFilename);
     }
 }
