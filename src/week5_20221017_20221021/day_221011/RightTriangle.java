0b53802b7b292687388f6233f465059c8c99bd5a
KoKwanwun <dkfajflsla@naver.com>
2022-10-17 09:20:42 +0900

    RightTriangle 코드 수정 (이중 for문으로 교체, 입력받도록 교체)

--- src/week5_20221017_20221021/day_221011/RightTriangle.java
+++ src/week5_20221017_20221021/day_221011/RightTriangle.java
@@ -1,10 +1,23 @@
 package week5_20221017_20221021.day_221011;
 
+import java.util.Scanner;
+
 public class RightTriangle {
+    void printRightTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+        RightTriangle rightTriangle = new RightTriangle();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        rightTriangle.printRightTriangle(n);
     }
 }
