2e48d46b1d314399f8778a1599f2cb85f9e5599c
KoKwanwun <dkfajflsla@naver.com>
2022-10-17 09:12:23 +0900

    RightTriangle 파일 추가 (직각삼각형 별찍기)

---
+++ src/week5_20221017_20221021/day_221011/RightTriangle.java
@@ -0,0 +1,10 @@
+package week5_20221017_20221021.day_221011;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
