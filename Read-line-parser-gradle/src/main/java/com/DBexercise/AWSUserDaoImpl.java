08444bbfd90239b3158a50c87108be19d3be84c4
KoKwanwun <dkfajflsla@naver.com>
2022-10-18 13:41:34 +0900

    Connection을 Abstract Method로 변경 (AWS, Local에 따라 사용할 수 있도록)

---
+++ Read-line-parser-gradle/src/main/java/com/DBexercise/AWSUserDaoImpl.java
@@ -0,0 +1,21 @@
+package com.DBexercise;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class AWSUserDaoImpl extends UserDaoAbstract{
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbName = env.get("DB_NAME");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbName, dbPassword);
+
+        return conn;
+    }
+}
---
+++ Read-line-parser-gradle/src/main/java/com/DBexercise/UserDaoAbstract.java
@@ -0,0 +1,72 @@
+package com.DBexercise;
+
+import com.DBexercise.domain.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public abstract class UserDaoAbstract {
+
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+        ps.close();
+        conn.close();
+    }
+
+    public User findbyId(String id) throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement("SELECT * from users where id = ?");
+        ps.setString(1, id);
+        ResultSet rs = ps.executeQuery();
+
+        rs.next();
+
+        User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return user;
+    }
+
+    public List<User> findAll() throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement("SELECT * from users");
+        ResultSet rs = ps.executeQuery();
+
+        List<User> userList = new ArrayList<>();
+
+        while(rs.next()){
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            userList.add(user);
+        }
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return userList;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        UserDaoAbstract userDao = new UserDaoAbstract();
+//        userDao.add(new User("7", "Eclipse", "880"));
+//
+//        List<User> userList = userDao.findAll();
+//        for (User user : userList) {
+//            System.out.println("ID : " + user.getId());
+//            System.out.println("NAME : " + user.getName());
+//            System.out.println("PASSWORD : " + user.getPassword());
+//        }
+    }
+}
