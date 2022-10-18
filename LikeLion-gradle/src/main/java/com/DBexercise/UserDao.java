586a0678fc8db8e1770903b0ccce6d63fc4f679d
KoKwanwun <dkfajflsla@naver.com>
2022-10-18 10:21:51 +0900

    자바와 DB 연동 (자바에서 Insert문, Select문 실행)

---
+++ LikeLion-gradle/src/main/java/com/DBexercise/UserDao.java
@@ -0,0 +1,63 @@
+package com.DBexercise;
+
+import com.DBexercise.domain.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UserDao {
+    public void add() throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbName = env.get("DB_NAME");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbName, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+        ps.setString(1, "3");
+        ps.setString(2, "TOEIC");
+        ps.setString(3, "1000");
+
+        ps.executeUpdate();
+        ps.close();
+        conn.close();
+    }
+
+    public List<User> findAll() throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbName = env.get("DB_NAME");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbName, dbPassword);
+        PreparedStatement ps = conn.prepareStatement("SELECT * from users");
+        ResultSet rs = ps.executeQuery();
+
+        List<User> userList = new ArrayList<>();
+
+        while(rs.next()){
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            userList.add(user);
+        }
+        rs.close();
+        ps.close();
+        conn.close();
+
+        return userList;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+//        userDao.add();
+        List<User> userList = userDao.findAll();
+        for (User user : userList) {
+            System.out.println("ID : " + user.getId());
+            System.out.println("NAME : " + user.getName());
+            System.out.println("PASSWORD : " + user.getPassword());
+        }
+    }
+}
