419a206b298ad01044be380035b52bb63f8d3cef
KoKwanwun <dkfajflsla@naver.com>
2022-10-18 16:01:21 +0900

    AwsConnectionMaker 클래스 생성 (객체 생성하여 getConnection사용 및 테스트)

---
+++ Read-line-parser-gradle/src/main/java/com/DAO/AwsConnectionMaker.java
@@ -0,0 +1,20 @@
+package com.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class AwsConnectionMaker {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbName = env.get("DB_NAME");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbName, dbPassword);
+
+        return conn;
+    }
+}
---
+++ Read-line-parser-gradle/src/test/java/com/DAO/UserDaoTest.java
@@ -0,0 +1,29 @@
+package com.DAO;
+
+import com.DAO.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+class UserDaoTest {
+    @Test
+    void addAndSelect() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        User user = new User("10", "Lotto", "6666");
+//        userDao.add(user);
+
+        User selectedUser = userDao.findbyId("10");
+        Assertions.assertEquals("Lotto", selectedUser.getName());
+    }
+
+    @Test
+    void addAndSelectAws() throws SQLException, ClassNotFoundException {
+        AWSUserDaoImpl userDao = new AWSUserDaoImpl();
+        User user = new User("10", "Lotto", "6666");
+//        userDao.add(user);
+
+        User selectedUser = userDao.findbyId("10");
+        Assertions.assertEquals("Lotto", selectedUser.getName());
+    }
+}
