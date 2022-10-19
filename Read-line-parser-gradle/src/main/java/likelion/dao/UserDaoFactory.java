e7a68800a9c25919914a76370ec55b91e8b857e7
KoKwanwun <dkfajflsla@naver.com>
2022-10-19 14:20:17 +0900

    UserDao Spring 적용

--- Read-line-parser-gradle/src/main/java/likelion/dao/UserDaoFactory.java
+++ Read-line-parser-gradle/src/main/java/likelion/dao/UserDaoFactory.java
@@ -1,13 +1,20 @@
 package likelion.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
     // Factory는 조립을 해줌 - DAO에서는
+    @Bean
     public UserDao awsUserDao() {
         AwsConnectionMaker awsConnectionMaker = new AwsConnectionMaker();
+        // context 재사용 하는 부분이 많은 코드
         UserDao userDao = new UserDao(awsConnectionMaker);
         return userDao;
     }
 
+    @Bean
     public UserDao localUserDao() {
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
--- Read-line-parser-gradle/src/test/java/likelion/dao/UserDaoTest.java
+++ Read-line-parser-gradle/src/test/java/likelion/dao/UserDaoTest.java
@@ -3,17 +3,30 @@ package likelion.dao;
 import likelion.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
+
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
 //        UserDao userDao = new UserDao(new AwsConnectionMaker());
-        // 위의 코드를 Factory를 적용
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        // Local에도 적용 가능
+//        Factory 적용
+//        UserDao userDao = new UserDaoFactory().awsUserDao();
+//        Local에도 적용 가능
 //        UserDao userDao = new UserDaoFactory().localUserDao();
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        UserDao userDao2 = context.getBean("awsUserDao", UserDao.class);
 
         User user = new User("10", "Lotto", "6666");
 //        userDao.add(user);
