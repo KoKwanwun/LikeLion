443a84b10cb8cd4dae3a198d28a8cc6a87076d4c
KoKwanwun <dkfajflsla@naver.com>
2022-10-20 13:31:32 +0900

    getCount()를 테스트하는 count() 테스트 Method 추가

--- Read-line-parser-gradle/src/test/java/likelion/dao/UserDaoTest.java
+++ Read-line-parser-gradle/src/test/java/likelion/dao/UserDaoTest.java
@@ -1,7 +1,6 @@
 package likelion.dao;
 
 import likelion.domain.User;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = UserDaoFactory.class)
@@ -20,18 +21,37 @@ class UserDaoTest {
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-//        UserDao userDao = new UserDao(new AwsConnectionMaker());
-//        Factory 적용
-//        UserDao userDao = new UserDaoFactory().awsUserDao();
-//        Local에도 적용 가능
-//        UserDao userDao = new UserDaoFactory().localUserDao();
         UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        UserDao userDao2 = context.getBean("awsUserDao", UserDao.class);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
 
-        User user = new User("10", "Lotto", "6666");
-//        userDao.add(user);
+        String id = "11";
+        User user = new User(id, "Lotto", "6666");
+        userDao.add(user);
+        assertEquals(1, userDao.getCount());
 
-        User selectedUser = userDao.findbyId("10");
-        Assertions.assertEquals("Lotto", selectedUser.getName());
+
+        User selectedUser = userDao.findbyId(id);
+        assertEquals("Lotto", selectedUser.getName());
+    }
+
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        User user1 = new User("1", "박성철", "1234");
+        User user2 = new User("2", "이길원", "2345");
+        User user3 = new User("3", "박범진", "3456");
+
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
     }
 }
