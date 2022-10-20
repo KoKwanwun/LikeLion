98a221aab80255c326cede3f38758340581b3b50
KoKwanwun <dkfajflsla@naver.com>
2022-10-20 14:08:27 +0900

    findById 예외조건 추가 (찾는 Id가 없을 경우 예외처리)

--- Read-line-parser-gradle/src/main/java/likelion/dao/UserDao.java
+++ Read-line-parser-gradle/src/main/java/likelion/dao/UserDao.java
@@ -1,6 +1,7 @@
 package likelion.dao;
 
 import likelion.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -37,13 +38,15 @@ public class UserDao {
         ps.setString(1, id);
         ResultSet rs = ps.executeQuery();
 
-        rs.next();
-
-        User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-
+        User user = null;
+        if(rs.next()){
+            user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+        }
         rs.close();
         ps.close();
         conn.close();
+
+        if (user == null) throw new EmptyResultDataAccessException(1);
 
         return user;
     }
