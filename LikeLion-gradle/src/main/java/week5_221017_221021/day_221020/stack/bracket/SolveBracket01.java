a3aef732d7cb6bf26299335446c6f6a5e6050626
KoKwanwun <dkfajflsla@naver.com>
2022-10-21 10:42:15 +0900

    SolveBracket01 코드 추가 (split을 이용한 괄호 풀기)

--- LikeLion-gradle/src/main/java/week5_221017_221021/day_221020/stack/bracket/SolveBracket01.java
+++ LikeLion-gradle/src/main/java/week5_221017_221021/day_221020/stack/bracket/SolveBracket01.java
@@ -8,4 +8,14 @@ public class SolveBracket01 {
         }
         return s.length() == 0;
     }
+
+    public boolean solution1(String s) {
+        // Stack 쓰지않고 괄호 풀기
+        while(s.indexOf("()") >= 0){
+            String[] splitted = s.split("\\(\\)");
+            s = String.join("", splitted);
+        }
+
+        return s.length() == 0;
+    }
 }
