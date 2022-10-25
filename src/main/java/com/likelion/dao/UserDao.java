3bafc48c0e9bb70d699851d6285669fa9278990c
KoKwanwun <dkfajflsla@naver.com>
2022-10-25 13:58:18 +0900

    getAll() 및 getAllTest() 추가

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 public class UserDao {
     private DataSource dataSource;
@@ -17,7 +18,7 @@ public class UserDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(User user) throws SQLException {
+    public void add(User user) {
         this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES(?,?,?);",
                 user.getId(), user.getName(), user.getPassword());
     }
@@ -35,11 +36,24 @@ public class UserDao {
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
-    public void deleteAll() throws SQLException {
+    public List<User> getAll() {
+        String sql = "SELECT * FROM users order by id";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"), rs.getString("name"),
+                        rs.getString("password"));
+                return user;
+            }
+        };
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public void deleteAll() {
         this.jdbcTemplate.update("delete from users");
     }
 
-    public int getCount() throws SQLException {
+    public int getCount() {
         return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 }
--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,21 +20,21 @@ class UserDaoTest {
 
     @Autowired
     private ApplicationContext context;
+    private UserDao userDao;
     private User user1;
     private User user2;
     private User user3;
 
     @BeforeEach
     void setUp() {
+        userDao = context.getBean("awsUserDao", UserDao.class);
         user1 = new User("1", "김삼성", "1234");
         user2 = new User("2", "강카카", "2345");
         user3 = new User("3", "박네이", "3456");
     }
 
     @Test
-    void addAndget() throws SQLException {
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-
+    void addAndget() {
         userDao.deleteAll();
         assertEquals(0, userDao.getCount());
 
@@ -41,5 +42,18 @@ class UserDaoTest {
         userDao.add(user2);
         userDao.add(user3);
         assertEquals(3, userDao.getCount());
+    }
+
+    @Test
+    void getAllTest() {
+        userDao.deleteAll();
+        List<User> users = userDao.getAll();
+        assertEquals(0, users.size());
+
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+        users = userDao.getAll();
+        assertEquals(3, users.size());
     }
 }
