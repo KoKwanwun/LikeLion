4cb289436535fc522f43b4daacb09aea047fbcb5
KoKwanwun <dkfajflsla@naver.com>
2022-10-25 11:46:37 +0900

    Test 코드 @BeforeEach 적용

--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -1,24 +1,45 @@
 package com.likelion.dao;
 
 import com.likelion.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    private ApplicationContext context;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    void setUp() {
+        user1 = new User("1", "김삼성", "1234");
+        user2 = new User("2", "강카카", "2345");
+        user3 = new User("3", "박네이", "3456");
+    }
+
     @Test
-    void addAndget() {
+    void addAndget() throws SQLException {
         UserDao userDao = new UserDao();
 
-        User user = new User("5", "김토익", "1234");
-        userDao.add(user);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
 
-        User selectedUser = userDao.findById("5");
-        assertEquals(user.getName(), selectedUser.getName());
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
     }
 }
