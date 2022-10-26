7c1a468aa23d135eaf631bcc08ae82f20e3e2896
KoKwanwun <dkfajflsla@naver.com>
2022-10-26 17:58:51 +0900

    SpringBootCoreGuide 추가

---
+++ SpringBootCoreGuide/src/main/java/com/springboot/springbootcoreguide/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.springboot.springbootcoreguide.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Kwanwun";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
+    }
+}
