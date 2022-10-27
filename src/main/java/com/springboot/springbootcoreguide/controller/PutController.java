81f206aff8853966ab3ca9741b502b8d6003c0a1
KoKwanwun <dkfajflsla@naver.com>
2022-10-27 21:33:29 +0900

    [Add] Get, Post, Put

---
+++ src/main/java/com/springboot/springbootcoreguide/controller/PutController.java
@@ -0,0 +1,20 @@
+package com.springboot.springbootcoreguide.controller;
+
+import com.springboot.springbootcoreguide.domain.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> putMemberDto(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+}
