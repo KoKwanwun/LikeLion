9c6f5de7b22e19cb301b6c96ed7ab5601ee1f7cb
KoKwanwun <dkfajflsla@naver.com>
2022-10-27 11:49:07 +0900

    [Add] Map으로 Request Param 받기

--- SpringBootCoreGuide/src/main/java/com/springboot/springbootcoreguide/controller/HelloController.java
+++ SpringBootCoreGuide/src/main/java/com/springboot/springbootcoreguide/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.springboot.springbootcoreguide.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
@@ -30,5 +32,16 @@ public class HelloController {
                                @RequestParam String email,
                                @RequestParam String organization) {
         return name + " " + email + " " + organization;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
     }
 }
