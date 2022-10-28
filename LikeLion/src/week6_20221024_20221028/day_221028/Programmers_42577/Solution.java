9f982f5044828a1f1967d19f1a0bd874087c43de
KoKwanwun <dkfajflsla@naver.com>
2022-10-28 10:59:47 +0900

    [Update] Programmers_42577 전화번호 목록 (정렬 주석 추가)

--- LikeLion/src/week6_20221024_20221028/day_221028/Programmers_42577/Solution.java
+++ LikeLion/src/week6_20221024_20221028/day_221028/Programmers_42577/Solution.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 정렬하여 인덱스 차이가 1인 것끼리만 비교 가능
+        /*
+         정렬하여 인덱스 차이가 1인 것끼리만 비교 가능
+         문자열 정렬이기 때문에 아래와 같이 정렬됨 (오름차순이 아닌 첫째자리부터 더 작은 수가 오는 것이 앞에)
+         ["119", "97674223", "1195524421"] -> ["119", "1195524421", "97674223"]
+         ["12","123","1235","567","88"] -> ["12","123","1235","567","88"]
+        */
         Arrays.sort(phone_book);
 
         // starsWith : phone_book[i]가 phone_book[i-1]로 시작하는지 확인
