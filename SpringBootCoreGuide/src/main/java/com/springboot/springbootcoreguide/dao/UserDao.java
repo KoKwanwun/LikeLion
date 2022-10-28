22aa629f22ce505e3f9eab0cbc10d7b2638d2771
KoKwanwun <dkfajflsla@naver.com>
2022-10-28 13:35:29 +0900

    [Add] UserDao, User

---
+++ SpringBootCoreGuide/src/main/java/com/springboot/springbootcoreguide/dao/UserDao.java
@@ -0,0 +1,54 @@
+package com.springboot.springbootcoreguide.dao;
+
+import com.springboot.springbootcoreguide.domain.dao.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Component
+public class UserDao {
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    RowMapper rowMapper = new RowMapper() {
+        @Override
+        public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"), rs.getString("name"),
+                    rs.getString("password"));
+            return user;
+        }
+    };
+
+    public void add(User user) {
+        this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    public void findById(String id) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        this.jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    public List<User> getAll() {
+        String sql = "SELECT * FROM users order by id";
+        return this.jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+}
