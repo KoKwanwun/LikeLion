c8d1496eed4642d0999ce34361b1b500709b6274
KoKwanwun <dkfajflsla@naver.com>
2022-10-28 14:16:12 +0900

    [Add] UserController : POST(user 등록), DELETE(유저 1명 삭제, 전체 유저 삭제) 완료

---
+++ SpringBootCoreGuide/src/main/java/com/springboot/springbootcoreguide/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.springboot.springbootcoreguide.controller;
+
+import com.springboot.springbootcoreguide.dao.UserDao;
+import com.springboot.springbootcoreguide.domain.dao.User;
+import com.springboot.springbootcoreguide.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+    private final UserDao userDao;
+
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @PostMapping("/user")
+    public String add(@RequestBody User user) {
+        userDao.add(user);
+        return String.format("%s번에 정보가 등록되었습니다.", user.getId());
+    }
+
+    @DeleteMapping(value = "/user/{id}")
+    public String deleteById(@PathVariable String id) {
+        userDao.deleteById(id);
+        return String.format("%s번의 정보가 삭제되었습니다.", id);
+    }
+
+    @DeleteMapping(value = "/user/all")
+    public String deleteAll() {
+        userDao.deleteAll();
+        return "모든 정보가 삭제되었습니다.";
+    }
+}
