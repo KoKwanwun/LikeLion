676279c692ca69ace5c74e01d4447c1828a331f6
KoKwanwun <dkfajflsla@naver.com>
2022-10-31 09:45:38 +0900

    [Update] Programmers_42840 모의고사 완료(주석 추가)

--- LikeLion/src/week7_20221031_20221103/day_221031/Programmers_42840/Solution.java
+++ LikeLion/src/week7_20221031_20221103/day_221031/Programmers_42840/Solution.java
@@ -10,6 +10,7 @@ class Solution {
 
         int[] correctValue = {0, 0, 0};
 
+        // %연산자를 활용하여 각 수포자별 정답 개수 갱신
         for (int i = 0; i < answers.length; i++) {
             if(answers[i] == person1[i % person1.length]){
                 correctValue[0]++;
@@ -22,12 +23,13 @@ class Solution {
             }
         }
 
+        // 수포자 중 가장 큰 정답 개수 구하기
         int maxValue = -1;
-
         for (int i = 0; i < 3; i++) {
             if(maxValue < correctValue[i])  maxValue = correctValue[i];
         }
 
+        // list로 점수가 가장 높은 수포자 넣기
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             if(maxValue == correctValue[i]){
@@ -35,6 +37,7 @@ class Solution {
             }
         }
 
+        // 이후, 배열로 바꾸어 리턴
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
