b0469bc9f6411830e9b35a6494c796a3475ef974
KoKwanwun <dkfajflsla@naver.com>
2022-10-31 09:38:11 +0900

    [Add] Programmers_42840 리턴 형태 수정 필요

---
+++ LikeLion/src/week7_20221031_20221103/day_221031/Programmers_42840/Solution.java
@@ -0,0 +1,40 @@
+package week7_20221031_20221103.day_221031.Programmers_42840;
+
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] answers) {
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int[] correctValue = {0, 0, 0};
+
+        for (int i = 0; i < answers.length; i++) {
+            if(answers[i] == person1[i % person1.length]){
+                correctValue[0]++;
+            }
+            if(answers[i] == person2[i % person2.length]){
+                correctValue[1]++;
+            }
+            if(answers[i] == person3[i % person3.length]){
+                correctValue[2]++;
+            }
+        }
+
+        int maxValue = -1;
+
+        for (int i = 0; i < 3; i++) {
+            if(maxValue < correctValue[i])  maxValue = correctValue[i];
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if(maxValue == correctValue[i]){
+                list.add(i+1);
+            }
+        }
+
+        return list;
+    }
+}
