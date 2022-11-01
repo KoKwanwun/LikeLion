f975583bcad04adb86958d1f98f6e02f41c02e05
KoKwanwun <dkfajflsla@naver.com>
2022-11-01 10:30:08 +0900

    [Update] Template CallBack 적용 2단계 - Interface 도입 (소수 찾기 1,2,3방법)

--- LikeLion/src/week7_20221031_20221103/day_221101/Programmers_12921/IsPrimeStrategy.java
+++
@@ -1,5 +0,0 @@
-package week7_20221031_20221103.day_221101.Programmers_12921;
-
-public interface IsPrimeStrategy {
-    public boolean isPrime(int n);
-}
--- LikeLion/src/week7_20221031_20221103/day_221101/Programmers_12921/TemplateCallBackIsPrime.java
+++ LikeLion/src/week7_20221031_20221103/day_221101/Programmers_12921/TemplateCallBackIsPrime.java
@@ -1,13 +1,12 @@
 package week7_20221031_20221103.day_221101.Programmers_12921;
 
-public class TemplateCallBackIsPrime implements IsPrimeStrategy {
-    boolean someOperation(int i, int n){
-        return i < n;
-    }
-
-    public boolean isPrime(int n) {
+interface StatementStrategy {
+    boolean compare(int i, int n);
+}
+public class TemplateCallBackIsPrime {
+    public boolean isPrime(int n, StatementStrategy stmt) {
         // 2부터 n미만까지 나눴을 때
-        for (int i = 2; someOperation(i, n); i++) {
+        for (int i = 2; stmt.compare(i, n); i++) {
             if (n % i == 0) {
                 return false;
             }
