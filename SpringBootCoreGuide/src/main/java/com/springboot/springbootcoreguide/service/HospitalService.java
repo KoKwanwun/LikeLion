25b78578ee7546a0a59a65c6e6e73399adcc74ba
KoKwanwun <dkfajflsla@naver.com>
2022-11-02 14:02:54 +0900

    [Update] Transactional, Stream을 통해 시간 절약

--- SpringBootCoreGuide/src/main/java/com/springboot/springbootcoreguide/service/HospitalService.java
+++ SpringBootCoreGuide/src/main/java/com/springboot/springbootcoreguide/service/HospitalService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HospitalService {
@@ -22,23 +23,28 @@ public class HospitalService {
 
     @Transactional
     public int insertLargeVolumeHospitalData(String filename) {
-        int cnt = 0;
+        List<Hospital> hospitalList;
         try {
-            List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
-
-            for (Hospital hospital : hospitalList) {
-                try {
-                    hospitalDao.add(hospital);          // db에 insert
-                    cnt++;
-                } catch (Exception e) {
-                    System.out.printf("id:%d 레코드에 문제가 있습니다.", hospital.getId());
-                    throw new RuntimeException(e);
-                }
-            }
+            hospitalList = hospitalReadLineContext.readByLine(filename);
+            System.out.println("파싱이 끝났습니다.");
+            hospitalList.stream()
+                    .parallel()
+                    .forEach(hospital -> {
+                        try {
+                            this.hospitalDao.add(hospital); // db에 insert하는 구간
+                        } catch (Exception e) {
+                            System.out.printf("id:%d 레코드에 문제가 있습니다.\n",hospital.getId());
+                            throw new RuntimeException(e);
+                        }
+                    });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return cnt;
+        if (!Optional.of(hospitalList).isEmpty()) {
+            return hospitalList.size();
+        } else {
+            return 0;
+        }
     }
 }
