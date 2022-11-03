9037b911e968bb75a3d7f5dc2719993beed4a2cc
KoKwanwun <dkfajflsla@naver.com>
2022-11-03 10:11:17 +0900

    [Update] SieveOfEratosthenes 3단계 : 2를 제외한 2의 배수 제거 후 출력

--- LikeLion/src/week7_20221031_20221103/day_221103/SieveOfEratosthenes.java
+++ LikeLion/src/week7_20221031_20221103/day_221103/SieveOfEratosthenes.java
@@ -12,5 +12,19 @@ public class SieveOfEratosthenes {
         for (int i = 0; i < nums.length; i++) { // 2~50까지 채우기
             nums[i] = i + 2;
         }
+
+        // 2를 제외한 2의 배수 지우기
+        // 0 1 2 3 4 5 6 7 8  .. 48
+        // 2 3 4 5 6 7 8 9 10 ... 50
+        for (int i = 2; i < nums.length; i+=2) {
+            checks[i] = false;
+        }
+
+        // check를 참고해서 true인 nums[i]만 출력
+        for (int i = 0; i < nums.length; i++) {
+            if(checks[i]){
+                System.out.println(nums[i]);
+            }
+        }
     }
 }
