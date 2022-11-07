101f25b957bf62b601f0cd5f8e3cf87706a893f6
KoKwanwun <dkfajflsla@naver.com>
2022-11-07 10:30:26 +0900

    [Update] Programmers_12910 (나누어 떨어지는 숫자 배열) 완료 - PriorityQueue방법

--- LikeLion/src/week8_20221107_20221111/day_221107/Programmers_12910/Solution.java
+++ LikeLion/src/week8_20221107_20221111/day_221107/Programmers_12910/Solution.java
@@ -2,6 +2,7 @@ package week8_20221107_20221111.day_221107.Programmers_12910;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int[] arr, int divisor) {
@@ -27,6 +28,31 @@ class Solution {
         // 있는 경우, answer 리턴
         if(answer.length == 0)
             return notExistAnswer;
+        else{
+            return answer;
+        }
+    }
+
+    public int[] solution1(int[] arr, int divisor) {
+        // arr의 숫자 중 divisor로 나눴을 때 0인 경우만 PriorityQueue에 넣어주기
+        PriorityQueue<Integer> list = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % divisor == 0){
+                list.add(arr[i]);
+            }
+        }
+
+        // PriorityQueue의 값을 Array로 바꿔주고 정렬
+        int[] answer = new int[list.size()];
+        int idx = 0;
+        while(!list.isEmpty()){
+            answer[idx++] = list.poll();
+        }
+
+        // 나누어 떨어지는 원소가 없을 경우, [-1] 리턴
+        // 있는 경우, answer 리턴
+        if(answer.length == 0)
+            return new int[]{-1};
         else{
             return answer;
         }
