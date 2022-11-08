7aee917d82bc4844614dddeb8ddd49847b0e2e07
KoKwanwun <dkfajflsla@naver.com>
2022-11-08 11:31:17 +0900

    [Add] SpringBoot-mustache-bbs 복습 - Form에서 전송한 Data를 Post Controller에서 받기

---
+++ springboot-mustache-bbs-review/src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,12 @@
+package com.mustache.bbs.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class ArticleDto {
+    private String title;
+}
