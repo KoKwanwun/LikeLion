b957f89f277cc566a8f0c45a0a818f7fde18d7d6
KoKwanwun <dkfajflsla@naver.com>
2022-11-09 10:45:25 +0900

    [Add] SelectionSort BiFunction 적용 (오름차순, 내림차순)

---
+++ Algorithm/src/main/java/algorithm/sort/selectionsort/SelectionSortBifunction.java
@@ -0,0 +1,32 @@
+package algorithm.sort.selectionsort;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public class SelectionSortBifunction {
+    public int[] selectionSort(int[] arr, BiFunction<Integer, Integer, Boolean> stmt) {
+        for (int i = 0; i < arr.length; i++) {
+            int targetIdx = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(stmt.apply(arr[targetIdx], arr[j])) targetIdx = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[targetIdx];
+            arr[targetIdx] = temp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        SelectionSortInterface ss = new SelectionSortInterface();
+        int[] ascArr = ss.selectionSort(nums, (a, b) -> a > b);
+        System.out.println(Arrays.toString(ascArr));
+
+        int[] descArr = ss.selectionSort(nums, (a, b) -> a < b);
+        System.out.println(Arrays.toString(descArr));
+
+
+    }
+}
