a1216fe24a4d94d98d9ac42a11ec20c6ee8fa97d
KoKwanwun <dkfajflsla@naver.com>
2022-11-10 15:13:18 +0900

    [Update] 파일 이동

---
+++ src/main/java/com/mustache/bbs/SpringbootMustacheBbsReviewApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustacheBbsReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMustacheBbsReviewApplication.class, args);
+	}
+
+}
