e98024fa8703de1571f59647338f5d1133e05f80
KoKwanwun <dkfajflsla@naver.com>
2022-11-10 15:11:34 +0900

    Revert "[Add] 게시판 프로젝트 추가"
    This reverts commit b08f3f95554a9e697d5530ec86bc91b11bcd7ddd.

--- springboot-mustache-bbs-review/src/main/java/com/mustache/bbs/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mustache.bbs.repository;
-
-import com.mustache.bbs.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
--- springboot-mustache-bbs-review/src/test/java/com/mustache/bbs/SpringbootMustacheBbsReviewApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.mustache.bbs;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringbootMustacheBbsReviewApplicationTests {
-
-}
