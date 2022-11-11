840a046d9d4d4e79b45b88a5d9c662a464fcf32b
KoKwanwun <dkfajflsla@naver.com>
2022-11-11 13:46:51 +0900

    [Update] 병원 정보 Paging 적용

--- src/main/java/com/mustache/bbs/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalController.java
@@ -1,8 +1,9 @@
 package com.mustache.bbs.controller;
 
-import com.mustache.bbs.domain.Article;
 import com.mustache.bbs.domain.Hospital;
 import com.mustache.bbs.repository.HospitalRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +15,16 @@ import java.util.List;
 @RequestMapping("/hospitals")
 public class HospitalController {
 
-    public final HospitalRepository hospitalRepository;
+    private final HospitalRepository hospitalRepository;
 
     public HospitalController(HospitalRepository hospitalRepository) {
         this.hospitalRepository = hospitalRepository;
     }
 
     @GetMapping("")
-    public String hospitalList(Model model){
-        List<Hospital> hospitals = hospitalRepository.findAll();
+    public String hospitalList(Model model, Pageable pageable){
+        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
         model.addAttribute("hospitals", hospitals);
-        return "/hospitals/list";
+        return "hospitals/list";
     }
 }
