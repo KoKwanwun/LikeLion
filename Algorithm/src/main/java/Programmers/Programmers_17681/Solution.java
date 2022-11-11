fc889214696625a63d4d87c5e1807aec9c85bb6c
KoKwanwun <dkfajflsla@naver.com>
2022-11-11 10:23:36 +0900

    [Update] Programmers_17681 : [1차] 비밀지도 코드 개선(비트연산자, replaceAll)

--- Algorithm/src/main/java/Programmers/Programmers_17681/Solution.java
+++ Algorithm/src/main/java/Programmers/Programmers_17681/Solution.java
@@ -2,29 +2,21 @@ package Programmers.Programmers_17681;
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String binArr1;
-        String binArr2;
+        String orBit;
+        String binStr;
         String[] answer = new String[n];
-        String result;
 
         for (int i = 0; i < n; i++) {
             // 길이가 n인 2진수로 변환
-            String binStr1 = Integer.toBinaryString(arr1[i]);
-            String binStr2 = Integer.toBinaryString(arr2[i]);
-            binArr1 = "0".repeat(n - binStr1.length()) + binStr1;
-            binArr2 = "0".repeat(n - binStr2.length()) + binStr2;
+            orBit = Integer.toBinaryString(arr1[i] | arr2[i]);
+            binStr = "0".repeat(n - orBit.length()) + orBit;
 
-            // 각 자리수에 1이 하나라도 있다면 #, 그외는 공백
-            result = "";
-            for (int j = 0; j < n; j++) {
-                if(binArr1.charAt(j) == '1' || binArr2.charAt(j) == '1') {
-                    result += "#";
-                } else {
-                    result += " ";
-                }
-            }
+            // replaceAll로 #과 공백으로 변환
+            binStr = binStr.replaceAll("1", "#");
+            binStr = binStr.replaceAll("0", " ");
+
             // 결과를 배열에 넣기
-            answer[i] = result;
+            answer[i] = binStr;
         }
         return answer;
     }
