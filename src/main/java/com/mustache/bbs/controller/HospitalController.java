8dd79c79c4ead94d75b8630e2a81d614da345dc0
KoKwanwun <dkfajflsla@naver.com>
2022-11-11 13:24:11 +0900

    [Update] 병원 정보 List 출력

--- src/main/java/com/mustache/bbs/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalController.java
@@ -1,15 +1,29 @@
 package com.mustache.bbs.controller;
 
+import com.mustache.bbs.domain.Article;
+import com.mustache.bbs.domain.Hospital;
+import com.mustache.bbs.repository.HospitalRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/hospitals")
 public class HospitalController {
 
+    public final HospitalRepository hospitalRepository;
+
+    public HospitalController(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
     @GetMapping("")
-    public String hospitalList(){
-        return "";
+    public String hospitalList(Model model){
+        List<Hospital> hospitals = hospitalRepository.findAll();
+        model.addAttribute("hospitals", hospitals);
+        return "/hospitals/list";
     }
 }
--- src/main/java/com/mustache/bbs/domain/Hospital.java
+++ src/main/java/com/mustache/bbs/domain/Hospital.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "nation_wide_hospitals")
@@ -18,4 +20,19 @@ import javax.persistence.Table;
 public class Hospital {
     @Id
     private Integer id;
+    private String open_service_name;
+    private Integer open_local_government_code;
+    private String management_number;
+    private Timestamp license_date;
+    private Integer business_status;
+    private Integer business_status_code;
+    private String phone;
+    private String full_address;
+    private String road_name_address;
+    private String hospital_name;
+    private String business_type_name;
+    private Integer healthcare_provider_count;
+    private Integer patient_room_count;
+    private Integer total_number_of_beds;
+    private float total_area_size;
 }
