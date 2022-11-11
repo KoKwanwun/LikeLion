fe0851cc84174c9af78e507dd4aa1074177b53b4
KoKwanwun <dkfajflsla@naver.com>
2022-11-11 09:50:36 +0900

    [Update] n개의 자리수가 되도록 2진수 앞에 0추가

--- Algorithm/src/main/java/algorithm/decimalToBinary/Main.java
+++ Algorithm/src/main/java/algorithm/decimalToBinary/Main.java
@@ -3,6 +3,7 @@ package algorithm.decimalToBinary;
 public class Main {
     public static void main(String[] args) {
         int num = 9;
+        int n = 6;
         int tmp;
         String binaryNum = "";
 
@@ -12,6 +13,11 @@ public class Main {
             binaryNum += tmp;
         }
         binaryNum += num;
+
+        // n만큼의 길이가 되도록 0추가
+        while(binaryNum.length() < 6) {
+            binaryNum += "0";
+        }
 
         String result = "";
         for (int i = binaryNum.length() - 1; i >= 0; i--) {
