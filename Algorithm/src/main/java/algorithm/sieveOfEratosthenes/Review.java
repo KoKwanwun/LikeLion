1a5b1fbe46e3ea02216a524c7a894d692fbad77d
KoKwanwun <dkfajflsla@naver.com>
2022-11-14 09:54:40 +0900

    [Add] 에라토스테네스의 체 복습

---
+++ Algorithm/src/main/java/algorithm/sieveOfEratosthenes/Review.java
@@ -0,0 +1,24 @@
+package algorithm.sieveOfEratosthenes;
+
+public class Review {
+    public static void main(String[] args) {
+        int size = 50;
+        int[] nums = new int[size-1];
+        boolean[] checks = new boolean[size-1];
+        int multipleOf;
+
+        // nums(2~50), check(true) 넣기
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 2;
+            checks[i] = true;
+        }
+
+        // 본인을 제외한 배수에 false 넣기
+        for (int i = 0; i < nums.length; i++) {
+            multipleOf = nums[i];
+            for (int j = multipleOf + i; j < nums.length; j+=multipleOf) {
+                checks[j] = false;
+            }
+        }
+    }
+}
