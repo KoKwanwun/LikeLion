9ce0890ccad4906bba1e41bb685b39f8ba097582
KoKwanwun <dkfajflsla@naver.com>
2022-11-14 13:29:47 +0900

    [Update] 경기도 수원시 피부과 쿼리문 주석으로 추가

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -3,5 +3,13 @@ package com.mustache.bbs.repository;
 import com.mustache.bbs.domain.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    /*
+    경기도 수원시 피부과 쿼리문
+    SELECT * FROM nation_wide_hospitals
+        WHERE road_name_address LIKE '경기도 수원시%'
+            AND hospital_name LIKE '%피부%';
+     */
 }
