75b325175f5960439232989fc0223629be3ee0c0
KoKwanwun <dkfajflsla@naver.com>
2022-11-15 13:51:00 +0900

    [Add] HospitalService 추가 : Repository를 Service가 한번 거치도록(businessStatusCode에 맞는 이름을 추가)

---
+++ src/main/java/com/mustache/bbs/service/HospitalService.java
@@ -0,0 +1,37 @@
+package com.mustache.bbs.service;
+
+import com.mustache.bbs.domain.Hospital;
+import com.mustache.bbs.dto.HospitalResponse;
+import com.mustache.bbs.repository.HospitalRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HospitalService {
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    public HospitalResponse getHospital(Integer id) {
+        Optional<Hospital> optHospital = hospitalRepository.findById(id);   // Entity
+        Hospital hospital = optHospital.get();
+        HospitalResponse hospitalResponse = Hospital.of(hospital); // DTO
+        
+        // code에 맞는 이름으로 변경
+        switch (hospitalResponse.getBusiness_status_code()){
+            case 13:
+                hospitalResponse.setBusinessStatusName("영업중");
+                break;
+            case 3:
+                hospitalResponse.setBusinessStatusName("폐업");
+                break;
+            default:
+                hospitalResponse.setBusinessStatusName(String.valueOf(hospital.getBusiness_status_code()));
+        }
+
+        return hospitalResponse;
+    }
+}
