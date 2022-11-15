fa3667ae0d6133ca08d69bcd15fe8e72c26e97da
KoKwanwun <dkfajflsla@naver.com>
2022-11-15 13:51:43 +0900

    [Update] api를 생성할 때 businessStatusCode에 맞는 이름을 추가로 넣도록 수정 (Service를 한번 거치도록)

--- src/main/java/com/mustache/bbs/controller/HospitalRestController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalRestController.java
@@ -2,7 +2,7 @@ package com.mustache.bbs.controller;
 
 import com.mustache.bbs.domain.Hospital;
 import com.mustache.bbs.dto.HospitalResponse;
-import com.mustache.bbs.repository.HospitalRepository;
+import com.mustache.bbs.service.HospitalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,16 +15,15 @@ import java.util.Optional;
 @RequestMapping("/api/v1/hospitals")
 public class HospitalRestController {
 
-    private final HospitalRepository hospitalRepository;
+    private final HospitalService hospitalService;
 
-    public HospitalRestController(HospitalRepository hospitalRepository) {
-        this.hospitalRepository = hospitalRepository;
+    public HospitalRestController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) {
-        Optional<Hospital> optHospital = hospitalRepository.findById(id);
-        HospitalResponse hospitalResponse = Hospital.of(optHospital.get());
+        HospitalResponse hospitalResponse = hospitalService.getHospital(id); // DTO
         return ResponseEntity.ok().body(hospitalResponse);
     }
 }
