e74dd82dc0d45dace5303912156b26fb5c536496
KoKwanwun <dkfajflsla@naver.com>
2022-11-17 10:17:12 +0900

    [Add] Quick Sort 완료 (Array 사용)

--- Algorithm/src/main/java/algorithm/sort/quicksort/SeparationToTwoGroups.java
+++ Algorithm/src/main/java/algorithm/sort/quicksort/SeparationToTwoGroups.java
@@ -5,17 +5,16 @@ import java.util.Arrays;
 public class SeparationToTwoGroups {
 
     public int[] sort(int[] arr, int startIdx, int endIdx) {
-        int pivot = arr[arr.length / 2];
-        int leftIdx = 0;
-        int rightIdx = arr.length - 1;
+        int pivot = arr[(startIdx + endIdx) / 2];
+        int leftIdx = startIdx;
+        int rightIdx = endIdx;
 
-        while(true) {
-            while (arr[leftIdx] < pivot) leftIdx += 1;
-            while (arr[rightIdx] > pivot) rightIdx += 1;
+        // pivot을 기준으로 교환
+        while(leftIdx <= rightIdx){
+            while(arr[leftIdx] < pivot) leftIdx += 1;
+            while(arr[rightIdx] > pivot) rightIdx -= 1;
 
-            if(leftIdx > rightIdx) {
-                break;
-            }else {
+            if(leftIdx <= rightIdx){
                 int temp = arr[leftIdx];
                 arr[leftIdx] = arr[rightIdx];
                 arr[rightIdx] = temp;
@@ -23,7 +22,11 @@ public class SeparationToTwoGroups {
                 rightIdx -= 1;
             }
         }
-        
+
+        // 재귀 사용
+        if(startIdx < rightIdx) sort(arr, startIdx, rightIdx);
+        if(leftIdx < endIdx) sort(arr, leftIdx, endIdx);
+
         return arr;
     }
 
@@ -31,6 +34,6 @@ public class SeparationToTwoGroups {
         var arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
         SeparationToTwoGroups st = new SeparationToTwoGroups();
 
-        System.out.println(Arrays.toString(st.sort(arr, 0, arr.length)));
+        System.out.println(Arrays.toString(st.sort(arr, 0, arr.length - 1)));
     }
 }
