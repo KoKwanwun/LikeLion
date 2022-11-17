e7ccb3732b1337d0afe5cf84df9f81f99e66c7ad
KoKwanwun <dkfajflsla@naver.com>
2022-11-17 14:34:19 +0900

    [Update] Username 중복 체크 기능 추가 (중복이면 저장X)

--- src/main/java/com/mustache/bbs/service/UserService.java
+++ src/main/java/com/mustache/bbs/service/UserService.java
@@ -29,7 +29,14 @@ public class UserService {
     }
 
     public UserResponse addUser(UserRequest dto) {
-        User savedUser = userRepository.save(dto.toEntity());
-        return new UserResponse(savedUser.getUsername(), "가입이 완료되었습니다.");
+        Optional<User> selectedUser = userRepository.findByUsername(dto.getUsername());
+
+        if(selectedUser.isEmpty()){
+            User savedUser = userRepository.save(dto.toEntity());
+            return new UserResponse(savedUser.getId(), savedUser.getUsername(), "가입이 완료되었습니다.");
+        } else {
+            return new UserResponse(null, dto.getUsername(), "해당 username은 이미 존재합니다.");
+        }
+
     }
 }
