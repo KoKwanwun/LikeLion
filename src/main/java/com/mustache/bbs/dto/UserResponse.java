36a68c8ebae13b241cc0c86efa4dc6a38b6ed07c
KoKwanwun <dkfajflsla@naver.com>
2022-11-17 14:33:33 +0900

    [Update] UserId까지 리턴하도록 하기 위해 삭제

--- src/main/java/com/mustache/bbs/dto/UserResponse.java
+++ src/main/java/com/mustache/bbs/dto/UserResponse.java
@@ -13,9 +13,4 @@ public class UserResponse {
     private Long id;
     private String username;
     private String message;
-
-    public UserResponse(String username, String message) {
-        this.username = username;
-        this.message = message;
-    }
 }
