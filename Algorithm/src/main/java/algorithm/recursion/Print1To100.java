a555d5b3f982413c4f6205b3b2a301433e109cff
KoKwanwun <dkfajflsla@naver.com>
2022-11-22 09:21:28 +0900

    [Update] 재귀 (1~100 출력) - 오름차순으로 출력하도록 변경

--- Algorithm/src/main/java/algorithm/recursion/Print1To100.java
+++ Algorithm/src/main/java/algorithm/recursion/Print1To100.java
@@ -2,14 +2,14 @@ package algorithm.recursion;
 
 public class Print1To100 {
     public static void print1To100(int num) {
-        if(num < 1){
+        if(num > 100){
             return;
         }
         System.out.println(num);
-        print1To100(num - 1);
+        print1To100(num + 1);
     }
 
     public static void main(String[] args) {
-        print1To100(100);
+        print1To100(1);
     }
 }
