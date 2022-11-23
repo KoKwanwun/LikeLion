e345c1beae5569e817298a58a9191ca504c4388c
KoKwanwun <dkfajflsla@naver.com>
2022-11-23 10:21:56 +0900

    [Update] fibonacci (재귀X) 주석 추가

--- Algorithm/src/main/java/algorithm/fibonacci/NotRecursion.java
+++ Algorithm/src/main/java/algorithm/fibonacci/NotRecursion.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class NotRecursion {
     public static void main(String[] args) {
+        // 값 입력받기
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        // n크기 배열 생성
         int[] arr = new int[n];
 
+        // i가 0 또는 1인 경우, 1을 넣고 나머지는 피보나치 공식 적용
         for (int i = 0; i < n; i++) {
             if(i == 0 || i == 1){
                 arr[i] = 1;
@@ -15,6 +19,7 @@ public class NotRecursion {
                 arr[i] = arr[i-1] + arr[i-2];
             }
         }
+        // 마지막 값 출력
         System.out.println(arr[n-1]);
     }
 }
