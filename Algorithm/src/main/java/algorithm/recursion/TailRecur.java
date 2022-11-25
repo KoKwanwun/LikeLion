54d6d200906a98fc1bcbd1333f576bfd33d13f06
KoKwanwun <dkfajflsla@naver.com>
2022-11-25 10:46:28 +0900

    [Update] 재귀 코드 수정(꼬리재귀 제거)

--- Algorithm/src/main/java/algorithm/recursion/TailRecur.java
+++ Algorithm/src/main/java/algorithm/recursion/TailRecur.java
@@ -2,10 +2,11 @@ package algorithm.recursion;
 
 public class TailRecur {
     public static void recur(int n) {
-        if (n <= 0) return;
-        recur(n - 1);
-        System.out.println(n);
-        recur(n - 2);
+        while(n > 0) {
+            recur(n - 1);
+            System.out.println(n);
+            n = n - 2;
+        }
     }
     public static void main(String[] args) {
         recur(3);
