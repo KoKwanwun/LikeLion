631d24e7bcf9a82ef68d40136c280462e0770b9e
KoKwanwun <dkfajflsla@naver.com>
2022-11-28 10:49:04 +0900

    [Update] 조합 - 재귀 활용 : 메소드명 수정 (알파벳 N자릿수)

--- Algorithm/src/main/java/algorithm/combination/AlphabetRecur.java
+++ Algorithm/src/main/java/algorithm/combination/AlphabetRecur.java
@@ -4,24 +4,24 @@ public class AlphabetRecur {
     public static void main(String[] args) {
         // 한 자리 수
         System.out.println("한 자리 수");
-        Alphabet(1, "");
+        alphabet(1, "");
 
         // 두 자리 수
         System.out.println("두 자리 수");
-        Alphabet(2, "");
+        alphabet(2, "");
 
         // 세 자리 수
         System.out.println("세 자리 수");
-        Alphabet(3, "");
+        alphabet(3, "");
     }
 
-    private static void Alphabet(int digitNum, String str) {
+    private static void alphabet(int digitNum, String str) {
         if(digitNum <= 0){
             System.out.println(str);
             return;
         }
         for (char c = 'A'; c <= 'Z'; c++) {
-            Alphabet(digitNum - 1, str + c);
+            alphabet(digitNum - 1, str + c);
         }
     }
 }
