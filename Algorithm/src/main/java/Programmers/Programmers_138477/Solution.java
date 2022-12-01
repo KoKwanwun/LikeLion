12c5c406730aeec5ccb8d96f4353e27e12cb32ca
KoKwanwun <dkfajflsla@naver.com>
2022-12-01 09:26:04 +0900

    [Add] Programmers_138477 명예의 전당(1) 완료

---
+++ Algorithm/src/main/java/Programmers/Programmers_138477/Solution.java
@@ -0,0 +1,36 @@
+package Programmers.Programmers_138477;
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        // k가 score의 길이보다 클 경우 방지
+        if(k > score.length){
+            k = score.length;
+        }
+
+        int[] answer = new int[score.length];
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // k의 크기까지는 pq에 그냥 넣고 각 단계마다 최소값을 answer에 넣기
+        for (int i = 0; i < k; i++) {
+            pq.add(score[i]);
+            answer[i] = pq.peek();
+        }
+
+        // score값이 pq의 최소값보다 작다면 추가하지 않고 최소값을 answer에 넣기
+        // 아니라면, pq의 최소값을 꺼낸 후, score를 새롭게 추가하고 그 중 최소값을 answer에 넣기
+        for (int i = k; i < score.length; i++) {
+            if(score[i] < pq.peek()) {
+                answer[i] = pq.peek();
+            } else{
+                pq.poll();
+                pq.add(score[i]);
+                answer[i] = pq.peek();
+            }
+        }
+
+        return answer;
+    }
+}
