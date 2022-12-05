519ec2673deb2ee4b3ac95adc3d3222235bf4558
KoKwanwun <dkfajflsla@naver.com>
2022-12-05 13:07:45 +0900

    [Add] TokenFilter 추가

---
+++ src/main/java/com/hospitalreview/configuration/JwtTokenFilter.java
@@ -0,0 +1,36 @@
+package com.hospitalreview.configuration;
+
+import com.hospitalreview.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // 권한 주거나 안주기
+        // 현재는 모두 닫힌 상태(SecurityConfig의 authenticated에서)
+        // 아래 코드를 거치면 권한 부여해줌
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", null, List.of(new SimpleGrantedAuthority("USER")));
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);  // 권한 부여
+        filterChain.doFilter(request, response);
+    }
+}
