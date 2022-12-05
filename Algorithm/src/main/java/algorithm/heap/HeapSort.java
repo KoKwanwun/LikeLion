f359d7f53b681a3ba707a709fd3be459f731bfba
KoKwanwun <dkfajflsla@naver.com>
2022-12-05 09:44:33 +0900

    [Update] Heap Sort 구현중 (최하위 노드 2개가 전체 노드 중 1, 2번째로 큰 값이라면 예외 발생)

--- Algorithm/src/main/java/algorithm/heap/HeapSort.java
+++ Algorithm/src/main/java/algorithm/heap/HeapSort.java
@@ -1,7 +1,9 @@
 package algorithm.heap;
 
+import java.util.Arrays;
+
 public class HeapSort {
-    public static int[] sort(int[] arr, int x, int y) {
+    public static int[] swap(int[] arr, int x, int y) {
         int tmp = arr[x];
         arr[x] = arr[y];
         arr[y] = tmp;
@@ -11,10 +13,18 @@ public class HeapSort {
 
     // 부모노드가 자식보다 항상 크거나 항상 작은 이진트리
     public static void main(String[] args) {
-        int[] arr = new int[]{6, 5, 7, 8};
+        int[] arr = new int[]{9, 8, 5, 7, 3, 2, 4, 10, 11};
 
-        for (int i = arr.length-1; i >= 0; i--) {
-            
+        for (int i = arr.length-1; i > 0; i--) {
+            if(i <= 2 & arr[i] > arr[0]){
+                swap(arr, i, 0);
+            }else if(i % 2 == 0 & arr[i] > arr[(i-2)/2]){
+                swap(arr, i, (i-2)/2);
+            }else if(i % 2 == 1 & arr[i] > arr[(i-1)/2]){
+                swap(arr, i, (i-1)/2);
+            }
+
+            System.out.println(Arrays.toString(arr));
         }
     }
 }
