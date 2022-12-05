be97d7f97ea1b324780623b6fbf20d23bd66de34
KoKwanwun <dkfajflsla@naver.com>
2022-12-05 14:12:18 +0900

    [Update] TokenFilter에 접근 제한 걸기

--- src/main/java/com/hospitalreview/configuration/JwtTokenFilter.java
+++ src/main/java/com/hospitalreview/configuration/JwtTokenFilter.java
@@ -1,7 +1,10 @@
 package com.hospitalreview.configuration;
 
 import com.hospitalreview.service.UserService;
+import com.hospitalreview.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +20,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final UserService userService;
@@ -25,9 +29,35 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // 권한 주거나 안주기
-        // 현재는 모두 닫힌 상태(SecurityConfig의 authenticated에서)
-        // 아래 코드를 거치면 권한 부여해줌
+        // 권한이 닫힌 상태에서 조건에 따라 권한을 줄지 말지 선택
+
+        // Header의 Authorization 가져오기
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorization:{}", authorizationHeader);
+        
+        // 토큰이 없거나 Bearer로 시작하지 않는 경우
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // token 분리
+        String token;
+        try {
+            token = authorizationHeader.split(" ")[1];
+        } catch (Exception e) {
+            log.info("token 추출에 실패했습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 토큰 유효 기간이 만료되었을 경우
+        if(JwtTokenUtil.isExpired(token, secretKey)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        
+        // 권한 부여
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", null, List.of(new SimpleGrantedAuthority("USER")));
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);  // 권한 부여
--- src/main/java/com/hospitalreview/utils/JwtTokenUtil.java
+++ src/main/java/com/hospitalreview/utils/JwtTokenUtil.java
@@ -7,6 +7,18 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtTokenUtil {
+
+    // secret를 사용하여 token을 parser
+    private static Claims extractClaims(String token, String key) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+
+    // token 사용기간이 유효한지 Check
+    public static boolean isExpired(String token, String key) {
+        Date expiredDate = extractClaims(token, key).getExpiration(); // expire timestamp를 return함
+        return expiredDate.before(new Date()); // 현재보다 전인지 check를 합니다.
+    }
+
     public static String createToken(String userName, String key, long expireTime) {
         Claims claims = Jwts.claims();  // 일종의 map
         claims.put("userName", userName);
