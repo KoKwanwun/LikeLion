a6ae9a4915b2a2500cfd3328213ae44ceae63e73
KoKwanwun <dkfajflsla@naver.com>
2022-12-14 09:16:09 +0900

    [Update] From부터 끝까지 더하는 2차원 배열 추가

--- Algorithm/src/main/java/algorithm/dp/OptimalStrategy.java
+++ Algorithm/src/main/java/algorithm/dp/OptimalStrategy.java
@@ -25,5 +25,28 @@ public class OptimalStrategy {
     public static void main(String[] args) {
         int[] arr = {2, 7, 40, 19};
         Pair1[][] dp = new Pair1[arr.length][arr.length];
+
+        // 숫자가 써있는 동전이 1개만 있는 경우
+        // 2 or 7 or 40 or 19
+        for (int i = 0; i < arr.length; i++) {
+            //0,0 1,1 2,2 3,3
+            // 내가 왼쪽
+            dp[i][i] = new Pair1(arr[i], 0);
+        }
+
+        // From부터 To까지 더하기
+        int[][] sumDp = new int[arr.length][arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if(i == j){
+                    sumDp[i][j] = arr[j];
+                    continue;
+                }
+                sumDp[i][j] = sumDp[i][j-1] + arr[j];
+            }
+        }
+
+        System.out.println(Arrays.deepToString(sumDp));
     }
 }
