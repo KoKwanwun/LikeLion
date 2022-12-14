de30e2d1fbf1fd20545676448f91e8cb8196775b
KoKwanwun <dkfajflsla@naver.com>
2022-12-14 10:13:54 +0900

    [Update] Pair1 클래스 생성으로 인한 삭제

--- Algorithm/src/main/java/algorithm/dp/OptimalStrategy.java
+++ Algorithm/src/main/java/algorithm/dp/OptimalStrategy.java
@@ -2,24 +2,6 @@ package algorithm.dp;
 
 import java.util.Arrays;
 
-class Pair1 {
-    int left;
-    int right;
-
-    public Pair1(int left, int right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    @Override
-    public String toString() {
-        return "Pair1{" +
-                "left=" + left +
-                ", right=" + right +
-                '}';
-    }
-}
-
 public class OptimalStrategy {
 
     public static void main(String[] args) {
