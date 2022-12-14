b4d2af7544c616c2f04a1e9c3886cb9a85744ad0
KoKwanwun <dkfajflsla@naver.com>
2022-12-14 10:14:11 +0900

    [Add] Pair1 추가

---
+++ Algorithm/src/main/java/algorithm/dp/Pair1.java
@@ -0,0 +1,27 @@
+package algorithm.dp;
+
+class Pair1 {
+    int left;
+    int right;
+
+    public Pair1(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair1{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
